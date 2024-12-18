da7df477b23525c6a3cb931ad0269ffd67d49927
Andreas Salhus Bakseter <141913422+baksetercx@users.noreply.github.com>
2024-12-18 12:02:34 +0100

    Add comment about dynamic secrets

--- pkg/hashivault/manager.go
+++ pkg/hashivault/manager.go
@@ -56,6 +56,8 @@ func (m *manager) GetSecret(ctx context.Context, path string) (EvergreenSecretsF
 	return es.get, nil
 }
 
+// Wrapper around GetSecret that gets only a single secret at the given key.
+// NOTE: If you have dynamic secrets that might change, you should use GetSecret and call the returned function every time you need the secret.
 func (m *manager) GetSecretAtKey(ctx context.Context, path, key string) (string, error) {
 	secret, err := m.GetSecret(ctx, path)
 	if err != nil {
