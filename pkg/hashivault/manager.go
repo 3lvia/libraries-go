5e88e1c0394d929b786b9e0590feba127d7f0142
Andreas Salhus Bakseter <141913422+baksetercx@users.noreply.github.com>
2024-12-18 12:59:41 +0100

    Use correct format for function doc (#46)

--- pkg/hashivault/manager.go
+++ pkg/hashivault/manager.go
@@ -56,7 +56,7 @@ func (m *manager) GetSecret(ctx context.Context, path string) (EvergreenSecretsF
 	return es.get, nil
 }
 
-// Wrapper around GetSecret that gets only a single secret at the given key.
+// GetStaticSecretAtKey wraps the GetSecret function and only gets a single secret at the given key.
 // NOTE: If you have dynamic secrets that might change, you should use GetSecret and call the returned function every time you need the secret.
 func (m *manager) GetStaticSecretAtKey(ctx context.Context, path, key string) (string, error) {
 	secret, err := m.GetSecret(ctx, path)
