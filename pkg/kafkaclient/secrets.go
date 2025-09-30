d6d3ecfd21d9549871b5140a668031507e4f112e
Tony Olsen <tony.olsen@elvia.no>
2025-09-30 15:18:21 +0200

    Removed unused secrets

--- pkg/kafkaclient/secrets.go
+++ pkg/kafkaclient/secrets.go
@@ -38,21 +38,10 @@ func (k K8sSecrets) Get(ctx context.Context, system string) (*SecretConfigValues
 		registryKey:    m["schema_registry_key"].(string),
 		registrySecret: m["schema_registry_secret"].(string),
 
-		blobSasURI:                                     m["blobstore-sas-uri"].(string),
-		exceptionInvocationConnectionString:            m["exceptioninvocations-connection-string"].(string),
-		exceptionInvocationLowPriorityConnectionString: m["exceptioninvocations-low-priority-connection-string"].(string),
-		exceptionInvocationFailOverConnectionString:    m["exceptioninvocations-low-priority-failed-over-connection-string"].(string),
-		exceptionInvocationLowPriorityTopic:            m["exceptioninvocations-low-priority-topic"].(string),
-		exceptionInvocationsTopic:                      m["exceptioninvocations-topic"].(string),
+		blobSasURI: m["blobstore-sas-uri"].(string),
 
 		key:    m["key"].(string),
 		secret: m["secret"].(string),
-
-		successfulInvocationsConnectionString:            m["successfulinvocations-connection-string"].(string),
-		successfulInvocationsLowPriorityConnectionString: m["successfulinvocations-low-priority-connection-string"].(string),
-		successfulInvocationsFailOverConnectionString:    m["successfulinvocations-low-priority-failed-over-connection-string"].(string),
-		successfulInvocationsLowPriorityTopic:            m["successfulinvocations-low-priority-topic"].(string),
-		successfulInvocationsTopic:                       m["successfulinvocations-topic"].(string),
 
 		bootstrapServer:    mInfo["bootstrap-server"].(string),
 		environment:        mInfo["environment"].(string),
@@ -69,18 +58,6 @@ type SecretConfigValues struct {
 	key, secret string
 
 	blobSasURI string
-
-	exceptionInvocationConnectionString            string
-	exceptionInvocationLowPriorityConnectionString string
-	exceptionInvocationFailOverConnectionString    string
-	exceptionInvocationLowPriorityTopic            string
-	exceptionInvocationsTopic                      string
-
-	successfulInvocationsConnectionString            string
-	successfulInvocationsLowPriorityConnectionString string
-	successfulInvocationsFailOverConnectionString    string
-	successfulInvocationsLowPriorityTopic            string
-	successfulInvocationsTopic                       string
 
 	bootstrapServer    string
 	environment        string
